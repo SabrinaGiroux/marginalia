8589b5f39fce6d62b13b2aae3b98c2d281372338
SabrinaGiroux <188945085+SabrinaGiroux@users.noreply.github.com>
2026-04-02 17:38:56 -0400

    add prettier for formatting

--- marginalia/src/App.tsx
+++ marginalia/src/App.tsx
@@ -1,15 +1,13 @@
-
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <>
       <section id="center">
         <h1> Marginalia</h1>
       </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
