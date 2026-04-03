238edbdea2f36bc62da88f95ec7a8ce2851cb1c7
SabrinaGiroux <188945085+SabrinaGiroux@users.noreply.github.com>
2026-04-02 21:03:34 -0400

    add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from 'react-router';
 import { HomeScreen } from './pages/HomeScreen';
 import { BookScreen } from './pages/BookScreen';
+import { Header } from './components/Header';
 
 function App() {
   return (
+    <>
+    <Header/>
     <Routes>
       <Route path="/" element={<HomeScreen />} />
       <Route path="/books/:id" element={<BookScreen />} />
     </Routes>
+    </>
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+
+
+export function Header() {
+  return (
+    <header className="w-full mx-auto p-5 flex items-center border-b border-[#2a2a2a]">
+        <h1 className="text-xl font-semibold text-slate-100 p-1">Marginalia</h1>
+    </header>
+  );
+}
