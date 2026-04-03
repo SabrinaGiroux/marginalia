3f744b191d2c05af9b89ba992936f89287559e5d
SabrinaGiroux <188945085+SabrinaGiroux@users.noreply.github.com>
2026-04-03 14:44:46 -0400

    add functionality to add and list books with dexie

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -1,13 +1,15 @@
 import { BookList } from '../components/BookList';
-
-
+import { useLiveQuery } from 'dexie-react-hooks';
+import { db } from '../lib/db'
 
 export function HomeScreen() {
+  const books = useLiveQuery(() => db.books.toArray(), []);
+
   return (
     <>
       <section className="flex flex-col items-center justify-start py-10 gap-7">
         <h2 className="text-2xl text-gray-300"> Your Books </h2>
-        <BookList books={[]} />
+        <BookList books={books || []} />
       </section>
     </>
   );
