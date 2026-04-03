3d69410b110b398ac1a95eae0fe4a4aba34b5e19
SabrinaGiroux <188945085+SabrinaGiroux@users.noreply.github.com>
2026-04-03 15:49:43 -0400

    add NavBar with library icon to go to home screen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
+import { NavBar } from "./NavBar";
+
+
 export function Header() {
   return (
-    <header className="w-full mx-auto p-5 flex items-center border-b border-[#2a2a2a]">
+    <header className="w-full mx-auto p-5 flex items-center justify-between border-b border-[#2a2a2a]">
       <h1 className="text-xl font-semibold text-slate-100 p-1">Marginalia</h1>
+      <NavBar/>
     </header>
   );
 }
