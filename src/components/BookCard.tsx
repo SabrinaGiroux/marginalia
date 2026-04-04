e0d3a340189a2f362b8170eda522297f58688b77
SabrinaGiroux <188945085+SabrinaGiroux@users.noreply.github.com>
2026-04-04 09:36:03 -0400

    update BookCard styling

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -12,26 +12,13 @@ export function BookCard({ book }: { book: Book }) {
         <BookCover coverUrl={book.coverUrl} title={book.title} />
 
         {/* Book Details */}
-        <div className="flex flex-col">
-          <div> {book.title} </div>
-          <div> {book?.author || 'Unknown'} </div>
-          <StarRating rating={rating} />
-        </div>
-        {/* Book Details */}
-        <div className="flex flex-col">
-          <div> {book.title} </div>
-          <div> {book?.author || 'Unknown'} </div>
-          <div className="h-[14px] flex justify-center">
-            {rating > 0 &&
-              [1, 2, 3, 4, 5].map((n) => (
-                <span
-                  key={n}
-                  className={`text-[11px] ${n <= rating ? 'text-[#c8a96e]' : 'text-[#3a3530]'}`}
-                >
-                  ★
-                </span>
-              ))}
+        <div className="flex flex-col gap-5 mt-5">
+          <div className="flex flex-col gap-2">
+            <div> {book.title} </div>
+            <div> {book?.author || 'Unknown'} </div>
           </div>
+
+          <StarRating rating={rating} />
         </div>
       </NavLink>
     </li>
