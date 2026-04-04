7f90d4fbe4205d19cc37a2bf10a6f04d0084d8d9
SabrinaGiroux <188945085+SabrinaGiroux@users.noreply.github.com>
2026-04-04 08:54:05 -0400

    format code

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,16 @@
 import { NavLink } from 'react-router';
-import BookImg from '../assets/book-shelf-line.png'
+import BookImg from '../assets/book-shelf-line.png';
 
 export function NavBar() {
   return (
     <nav className="px-5">
-        <ul>
-            <li>
-                <NavLink to="/"><img src={BookImg} alt="Book Icon" /></NavLink>
-            </li>
-        </ul>
+      <ul>
+        <li>
+          <NavLink to="/">
+            <img src={BookImg} alt="Book Icon" />
+          </NavLink>
+        </li>
+      </ul>
     </nav>
-
   );
 }
