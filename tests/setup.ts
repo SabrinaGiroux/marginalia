488e3dc824247ddb7ab590fbd44e3625b0e4f846
SabrinaGiroux <188945085+SabrinaGiroux@users.noreply.github.com>
2026-04-06 22:38:20 -0400

    rename file to setup + add import for testing library

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,4 +1,6 @@
-// File necessary for testing DOM with bun
+import '@testing-library/jest-dom';
+
+// Code necessary for testing DOM with bun
 import { GlobalRegistrator } from '@happy-dom/global-registrator';
 
 GlobalRegistrator.register();
