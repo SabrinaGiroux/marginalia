f91f6e943c4ed869b849688da7437d5a1a4d4246
SabrinaGiroux <188945085+SabrinaGiroux@users.noreply.github.com>
2026-04-06 22:39:32 -0400

    add tests for BookList component

---
+++ tests/components/BookList.test.tsx
@@ -0,0 +1,41 @@
+import { render, screen } from '../test-utils';
+import { expect, describe, it } from 'bun:test';
+
+import { BookList } from '../../src/components/BookList';
+import type { Book } from '../../src/types/Book';
+
+describe('BookList Component', () => {
+  it('renders a list of books', () => {
+    const mockBooks: Book[] = [
+      {
+        id: 1,
+        title: 'Book One',
+        author: 'Author One',
+        shelf: 'reading',
+        rating: 4,
+        description: 'Description One',
+      },
+      {
+        id: 2,
+        title: 'Book Two',
+        author: 'Author Two',
+        shelf: 'read',
+        rating: 5,
+        description: 'Description Two',
+      },
+    ];
+
+    render(<BookList books={mockBooks} />);
+
+    // Check if the book titles are rendered
+    expect(screen.getAllByText('Book One')[0]).toBeInTheDocument();
+    expect(screen.getAllByText('Book Two')[0]).toBeInTheDocument();
+  });
+
+  it('renders the add book card', () => {
+    render(<BookList books={[]} />);
+
+    // Check for add book message only
+    expect(screen.getByText('ADD BOOK')).toBeInTheDocument();
+  });
+});
