935aae5dad9a71337b1e3c5759a5efe17eaee149
SabrinaGiroux <188945085+SabrinaGiroux@users.noreply.github.com>
2026-05-03 10:02:05 -0400

    make onAddBook optional

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -2,14 +2,14 @@ import type { Book } from '../types/Book';
 import { AddBookCard } from './AddBookCard';
 import { BookCard } from './BookCard';
 
-export function BookList({ books, onAddBook }: { books: Book[]; onAddBook: () => void }) {
+export function BookList({ books, onAddBook }: { books: Book[]; onAddBook?: () => void }) {
   return (
     <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 max-w-5xl mx-auto px-4 auto-rows-fr">
       {books.map((book) => (
         <BookCard key={book.id} book={book} />
       ))}
 
-      <AddBookCard onClick={onAddBook} />
+      {onAddBook && <AddBookCard onClick={onAddBook} />}
     </ul>
   );
 }
