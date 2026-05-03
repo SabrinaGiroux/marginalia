a0b43b1574743930d0c6dc62e35e9c94c7cd3f7f
SabrinaGiroux <188945085+SabrinaGiroux@users.noreply.github.com>
2026-05-03 09:52:35 -0400

    add a button to add new book

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -1,11 +1,15 @@
 import { BookList } from '../components/BookList';
 import { useLiveQuery } from 'dexie-react-hooks';
 import { db } from '../lib/db';
+import { useState } from 'react';
+import { AddBookModal } from '../components/AddBookModal';
 
 export function HomeScreen() {
   const books = useLiveQuery(() => db.books.toArray(), []);
   const isLoading = books === undefined;
   const error = books === null;
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <section className="flex flex-col items-center justify-start py-10 gap-7">
@@ -15,17 +19,23 @@ export function HomeScreen() {
 
         <button
           className="text-sm px-3 py-1 border border-[#3a3530] rounded-md text-gray-300 hover:border-[#c8a96e] transition"
-          onClick={() => {}}
+          onClick={() => setIsModalOpen(true)}
         >
           + Add New Book
         </button>
       </div>
 
+      {/* Books / Loading / Error */}
       {isLoading && <p className="text-gray-400">Loading books...</p>}
 
       {error && <p className="text-red-400">Could not load books</p>}
 
-      {!isLoading && !error && <BookList books={books || []} />}
+      {!isLoading && !error && (
+        <BookList books={books || []} onAddBook={() => setIsModalOpen(true)} />
+      )}
+
+      {/* Modal */}
+      {isModalOpen && <AddBookModal onClose={() => setIsModalOpen(false)} />}
     </section>
   );
 }
