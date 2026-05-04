9baf1109d86b305009669cd764cf11041659b931
SabrinaGiroux <188945085+SabrinaGiroux@users.noreply.github.com>
2026-05-04 08:59:08 -0400

    add function to download json file

--- src/lib/file.ts
+++ src/lib/file.ts
@@ -11,3 +11,18 @@ function formatDataForExport(books: Book[]) {
 
   return payload;
 }
+
+async function downloadJSON(data: unknown, filename: string) {
+  const blob = new Blob([JSON.stringify(data, null, 2)], {
+    type: 'application/json',
+  });
+
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = filename;
+  a.click();
+
+  URL.revokeObjectURL(url);
+}
