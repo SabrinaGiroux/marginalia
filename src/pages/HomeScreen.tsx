8a97cf7fd3313cb7e7996e24d12286d2d70cffbe
SabrinaGiroux <188945085+SabrinaGiroux@users.noreply.github.com>
2026-05-24 10:28:59 -0400

    update ui to add SearchBar to homescreen

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -3,6 +3,7 @@ import { useLiveQuery } from 'dexie-react-hooks';
 import { db } from '../lib/db';
 import { useState } from 'react';
 import { AddBookModal } from '../components/AddBookModal';
+import { SearchBar } from '../components/SearchBar';
 
 export function HomeScreen() {
   const books = useLiveQuery(() => db.books.toArray(), []);
@@ -12,14 +13,17 @@ export function HomeScreen() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <section className="flex flex-col items-center justify-start py-10 gap-7">
+    <section className="flex flex-col py-10 gap-7 max-w-5xl mx-auto px-4 w-full">
       {/* Header */}
-      <div className="flex items-center gap-4">
-        <h2 className="text-2xl">Your Books</h2>
+      <div className="flex justify-between w-full">
+        <div className="flex items-center gap-4">
+          <h2 className="text-2xl">Your Books</h2>
 
-        <button className="btn-primary" onClick={() => setIsModalOpen(true)}>
-          + Add New Book
-        </button>
+          <button className="btn-primary" onClick={() => setIsModalOpen(true)}>
+            + Add New Book
+          </button>
+        </div>
+        <SearchBar value="test" onChange={() => {}} />
       </div>
 
       {/* Books / Loading / Error */}
